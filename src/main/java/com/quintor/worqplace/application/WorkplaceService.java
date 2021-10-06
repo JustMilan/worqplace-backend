f3ead85b11d4b7708dcc6eef353f15f74d8d49b2
JustMilan <milandol321@gmail.com>
2021-10-06 22:12:46 +0200

    Add filter for available workplaces

--- src/main/java/com/quintor/worqplace/application/WorkplaceService.java
+++ src/main/java/com/quintor/worqplace/application/WorkplaceService.java
@@ -1,17 +1,15 @@
 package com.quintor.worqplace.application;
 
-import com.quintor.worqplace.application.dto.ReservationDTO;
 import com.quintor.worqplace.application.dto.WorkplaceAvailibilityDTO;
 import com.quintor.worqplace.application.dto.WorkplaceDTO;
-import com.quintor.worqplace.application.exceptions.ReservationNotFoundException;
 import com.quintor.worqplace.application.exceptions.WorkplaceNotFoundException;
 import com.quintor.worqplace.data.WorkplaceRepository;
-import com.quintor.worqplace.domain.Reservation;
 import com.quintor.worqplace.domain.Workplace;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,7 +17,7 @@ import java.util.stream.Collectors;
 @Transactional
 @AllArgsConstructor
 public class WorkplaceService {
-    private WorkplaceRepository workplaceRepository;
+    private final WorkplaceRepository workplaceRepository;
 
     public List<WorkplaceAvailibilityDTO> getAllWorkplaces() {
         return workplaceRepository.findAll().stream().map(WorkplaceAvailibilityDTO::new).collect(Collectors.toList());
@@ -29,5 +27,17 @@ public class WorkplaceService {
         Workplace workplace = workplaceRepository.findById(id).orElseThrow(
                 () -> new WorkplaceNotFoundException("Workplace " + id + " not found"));
         return new WorkplaceAvailibilityDTO(workplace);
+    }
+
+    public List<WorkplaceDTO> getAllAvailableWorkplaces() {
+        var allWorkplaces = workplaceRepository.findAll();
+        List<Workplace> available = new ArrayList<>();
+
+        for (Workplace workplace : allWorkplaces)
+            for (var timeslot : workplace.getTimeslots())
+                if (timeslot.getReservation() == null)
+                    available.add(workplace);
+
+        return available.stream().map(WorkplaceDTO::new).collect(Collectors.toList());
     }
 }
