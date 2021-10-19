d23bcd0d6ae850acab5984ce249769627512515a
JustMilan <milandol321@gmail.com>
2021-10-19 23:03:47 +0200

    Add constructor paramter checks for Employee

--- src/main/java/com/quintor/worqplace/domain/Employee.java
+++ src/main/java/com/quintor/worqplace/domain/Employee.java
@@ -20,4 +20,36 @@ public class Employee {
 
 	private String firstName;
 	private String lastName;
+
+	public Employee(String firstName, String lastName) {
+		setFirstName(firstName);
+		setLastName(lastName);
+	}
+
+	public void setFirstName(String firstName) {
+		char[] firstNameChars = firstName.toCharArray();
+
+		if (!Character.isUpperCase(firstNameChars[0]))
+			throw new RuntimeException("Name must start with a capital letter");
+
+		for (char c : firstNameChars)
+			if (!Character.isLetter(c))
+				throw new RuntimeException("first name must consist letters only");
+
+
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		char[] lastNameChars = lastName.toCharArray();
+
+		if (!Character.isUpperCase(lastNameChars[0]))
+			throw new RuntimeException("Name must start with a capital letter");
+
+		for (char c : lastNameChars)
+			if (!Character.isLetter(c))
+				throw new RuntimeException("last name must consist letters only");
+
+		this.lastName = lastName;
+	}
 }
