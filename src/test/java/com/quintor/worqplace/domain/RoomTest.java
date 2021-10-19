ee38983abe2a5ae325748f12b5acb0d8d3f7bd49
JustMilan <milandol321@gmail.com>
2021-10-20 00:13:54 +0200

    Create test for Room creation

--- src/test/java/com/quintor/worqplace/domain/RoomTest.java
+++ src/test/java/com/quintor/worqplace/domain/RoomTest.java
@@ -1,7 +1,26 @@
 package com.quintor.worqplace.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class RoomTest {
+	private long id;
+	private int floor;
+	private List<Workplace> workplaces;
 
+	@BeforeEach
+	void initialize() {
+		this.id = 1L;
+		this.floor = 1;
+		this.workplaces = List.of(new Workplace(), new Workplace());
+	}
+
+	@Test
+	void shouldCreateRoomCorrectly() {
+		assertDoesNotThrow(() -> new Room(id, floor, workplaces));
+	}
 }
