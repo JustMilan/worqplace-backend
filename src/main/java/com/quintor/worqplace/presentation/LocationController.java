03695e4ec1998354a7cb5a38876be6e371012024
sayshi <s.shirre@hotmail.com>
2021-10-21 23:00:29 +0200

    Add LocationController

---
+++ src/main/java/com/quintor/worqplace/presentation/LocationController.java
@@ -0,0 +1,29 @@
+package com.quintor.worqplace.presentation;
+
+import com.quintor.worqplace.application.LocationService;
+import com.quintor.worqplace.application.dto.location.LocationMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/locations")
+@AllArgsConstructor
+public class LocationController {
+    private final LocationService locationService;
+    private final LocationMapper locationMapper;
+
+    @CrossOrigin(origins = {"http://localhost:4200"})
+    @GetMapping
+    private ResponseEntity<?> getAllLocations() {
+        return new ResponseEntity<>(locationService.getAllLocations().stream().map(locationMapper::toLocationDTO)
+                .collect(Collectors.toList()), HttpStatus.OK);
+    }
+
+}
