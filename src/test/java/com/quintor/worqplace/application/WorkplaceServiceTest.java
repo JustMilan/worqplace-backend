e82c7aad754a967bef9756d37db7e8047571aa4e
JustMilan <milandol321@gmail.com>
2021-10-27 12:40:00 +0200

    Add empty tests for WorkplaceService

--- src/test/java/com/quintor/worqplace/application/WorkplaceServiceTest.java
+++ src/test/java/com/quintor/worqplace/application/WorkplaceServiceTest.java
@@ -1,7 +1,67 @@
 package com.quintor.worqplace.application;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.quintor.worqplace.data.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
 
 class WorkplaceServiceTest {
+	private RoomRepository roomRepository;
+	private LocationRepository locationRepository;
+	private EmployeeRepository employeeRepository;
+	private WorkplaceRepository workplaceRepository;
+	private ReservationRepository reservationRepository;
 
+	private RoomService roomService;
+	private EmployeeService employeeService;
+	private LocationService locationService;
+	private WorkplaceService workplaceService;
+	private ReservationService reservationService;
+
+	@BeforeEach
+	void initialize() {
+		this.roomRepository = mock(RoomRepository.class);
+		this.employeeRepository = mock(EmployeeRepository.class);
+		this.locationRepository = mock(LocationRepository.class);
+		this.workplaceRepository = mock(WorkplaceRepository.class);
+		this.reservationRepository = mock(ReservationRepository.class);
+
+		this.locationService = new LocationService(locationRepository);
+		this.employeeService = new EmployeeService(employeeRepository);
+		this.roomService = new RoomService(roomRepository, locationService, reservationService);
+		this.workplaceService = new WorkplaceService(workplaceRepository, locationService, reservationService);
+		this.reservationService = new ReservationService(employeeService, workplaceService, roomService, reservationRepository);
+	}
+
+	@Test
+	void getAllWorkplacesShouldReturnWorkplaces() {
+	}
+
+	@Test
+	void getWorkplaceByIdShouldReturnWorkplace() {
+	}
+
+	@Test
+	void getWorkplaceByIdShouldThrowIfNotFound() {
+	}
+
+	@Test
+	void getWorkplacesAvailabilityShouldReturnAvailableWorkplaces() {
+	}
+
+	@Test
+	void getWorkplacesAvailabilityShouldThrowIfTnvalidTime() {}
+
+	@Test
+	void getWorkplacesAvailabilityShouldThrowIfTnvalidDate() {}
+
+	@Test
+	void findWorkplacesByLocationIdShouldReturnLocations() {}
+
+	@Test
+	void isWorkplaceAvailableDuringDateAndTimeShouldReturnTrueIfItIsAvailable() {}
+
+	@Test
+	void isWorkplaceAvailableDuringDateAndTimeShouldReturnTrueIfThereAreNoReservations() {}
 }
