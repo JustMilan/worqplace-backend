d66fb37d568f316f3efe63b38cb43c41d6766abd
JustMilan <milandol321@gmail.com>
2021-10-27 12:05:55 +0200

    Add empty tests for RoomService

--- src/test/java/com/quintor/worqplace/application/RoomServiceTest.java
+++ src/test/java/com/quintor/worqplace/application/RoomServiceTest.java
@@ -1,7 +1,84 @@
 package com.quintor.worqplace.application;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.quintor.worqplace.data.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
 
 class RoomServiceTest {
+	private RoomRepository roomRepository;
+	private LocationRepository locationRepository;
+	private EmployeeRepository employeeRepository;
+	private WorkplaceRepository workplaceRepository;
+	private ReservationRepository reservationRepository;
 
+	private RoomService roomService;
+	private LocationService locationService;
+	private EmployeeService employeeService;
+	private WorkplaceService workplaceService;
+	private ReservationService reservationService;
+
+	@BeforeEach
+	void initialize() {
+		this.roomRepository = mock(RoomRepository.class);
+		this.employeeRepository = mock(EmployeeRepository.class);
+		this.locationRepository = mock(LocationRepository.class);
+		this.workplaceRepository = mock(WorkplaceRepository.class);
+		this.reservationRepository = mock(ReservationRepository.class);
+
+		this.locationService = new LocationService(locationRepository);
+		this.employeeService = new EmployeeService(employeeRepository);
+		this.workplaceService = new WorkplaceService(workplaceRepository, locationService, reservationService);
+		this.roomService = new RoomService(roomRepository, locationService, reservationService);
+		this.reservationService = new ReservationService(employeeService, workplaceService, roomService, reservationRepository);
+	}
+
+	@Test
+	void GetRoomByIdShouldReturnRoom() {
+	}
+
+	@Test
+	void getRoomByIdShouldThrowIfNotFound() {
+	}
+
+	@Test
+	void getRoomsAvailabilityForDateShouldReturnAvailableRooms() {
+	}
+
+	@Test
+	void getRoomsAvailabilityForDateShouldThrowIfDateIsInvalid() {
+	}
+
+	@Test
+	void getAvailableRoomsForDateAndTimeShouldReturnAvailableRooms() {
+	}
+
+	@Test
+	void getAvailableRoomsForDateAndTimeShouldThrowIfTimesAreInvalid() {
+	}
+
+	@Test
+	void getAvailableRoomsForDateAndTimeShouldThrowIfDateIsInvalid() {
+	}
+
+	@Test
+	void isRoomAvailableShouldReturnTrueIfRoomIsAvailable() {
+	}
+
+	@Test
+	void isRoomAvailableShouldReturnFalseIfRoomIsNotAvailable() {
+	}
+
+	@Test
+	void isRoomAvailableShouldThrowIfDateIsInvalid() {
+	}
+
+	@Test
+	void isRoomAvailableShouldThrowIfTimeIsInvalid() {
+	}
+
+	@Test
+	void findRoomsByLocationIdShouldReturnRooms() {
+	}
 }
