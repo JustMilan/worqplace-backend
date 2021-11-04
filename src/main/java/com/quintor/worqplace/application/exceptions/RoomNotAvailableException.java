d6cc21bf605b9b87045ab22b1fad52931d0bfabf
Gerson <gerson@makweb.nl>
2021-11-04 12:29:44 +0100

    added documentation to exceptions

--- src/main/java/com/quintor/worqplace/application/exceptions/RoomNotAvailableException.java
+++ src/main/java/com/quintor/worqplace/application/exceptions/RoomNotAvailableException.java
@@ -1,7 +1,16 @@
 package com.quintor.worqplace.application.exceptions;
 
+/**
+ * Exception that is thrown if the selected
+ * {@link com.quintor.worqplace.domain.Room room} is not available
+ * at the selected timeslot.
+ *
+ * @see com.quintor.worqplace.domain.Room Room
+ * @see com.quintor.worqplace.domain.Reservation Reservation
+ * @see com.quintor.worqplace.application.ReservationService ReservationService
+ */
 public class RoomNotAvailableException extends RuntimeException {
-    public RoomNotAvailableException() {
-        super("Room is not available");
-    }
+	public RoomNotAvailableException() {
+		super("Room is not available");
+	}
 }
