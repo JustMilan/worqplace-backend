86e47508f90fbcafdc543fb561fe7e6ca4e05be6
JustMilan <milandol321@gmail.com>
2021-11-24 20:12:06 +0100

    Add service layer for security

---
+++ src/main/java/com/quintor/worqplace/security/application/UserService.java
@@ -0,0 +1,38 @@
+package com.quintor.worqplace.security.application;
+
+import com.quintor.worqplace.security.data.SpringUserRepository;
+import com.quintor.worqplace.security.data.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+/**
+ * Implements UserDetailsService in order to make it usable
+ * as login/registration service for Spring.
+ * (see AuthenticationFilter)
+ */
+@Service
+@Transactional
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+	private final SpringUserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	public void register(String username, String password) {
+		String encodedPassword = this.passwordEncoder.encode(password);
+
+		User user = new User(username, encodedPassword);
+
+		this.userRepository.save(user);
+	}
+
+	@Override
+	public User loadUserByUsername(String username) {
+		return this.userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException(username));
+	}
+}
