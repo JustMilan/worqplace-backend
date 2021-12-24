5a323b6ed8880f0f1229ef4629eaf7b84f5eca01
JustMilan <milandol321@gmail.com>
2021-12-24 19:22:26 +0100

    Remove before each and add static util import

--- src/test/java/com/quintor/worqplace/application/util/DateTimeUtilsTest.java
+++ src/test/java/com/quintor/worqplace/application/util/DateTimeUtilsTest.java
@@ -4,101 +4,79 @@ import com.quintor.worqplace.application.exceptions.InvalidDayException;
 import com.quintor.worqplace.application.exceptions.InvalidStartAndEndTimeException;
 import com.quintor.worqplace.domain.Recurrence;
 import com.quintor.worqplace.domain.RecurrencePattern;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import static com.quintor.worqplace.application.util.DateTimeUtils.checkReservationDateTime;
+import static com.quintor.worqplace.application.util.DateTimeUtils.timeslotsOverlap;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DateTimeUtilsTest {
 
-	private LocalDate today;
-	private LocalDate tomorrow;
-	private LocalDate yesterday;
-	private LocalTime nine;
-	private LocalTime twelve;
-	private LocalTime one;
-	private LocalTime four;
-	private Recurrence noRecurrence;
-	private Recurrence dailyRecurrence;
-	private Recurrence weeklyRecurrence;
-	private Recurrence biweeklyRecurrence;
-	private Recurrence monthlyRecurrence;
-
-	@BeforeEach
-	void setUp() {
-		this.today = LocalDate.now();
-		this.tomorrow = LocalDate.now().plusDays(1);
-		this.yesterday = LocalDate.now().minusDays(1);
-		this.nine = LocalTime.of(9, 0);
-		this.twelve = LocalTime.of(12, 0);
-		this.one = LocalTime.of(13, 0);
-		this.four = LocalTime.of(16, 0);
-		this.noRecurrence = new Recurrence(false, RecurrencePattern.NONE);
-		this.dailyRecurrence = new Recurrence(true, RecurrencePattern.DAILY);
-		this.weeklyRecurrence = new Recurrence(true, RecurrencePattern.WEEKLY);
-		this.biweeklyRecurrence = new Recurrence(true, RecurrencePattern.BIWEEKLY);
-		this.monthlyRecurrence = new Recurrence(true, RecurrencePattern.MONTHLY);
-	}
+	private static final LocalDate TODAY = LocalDate.now();
+	private static final LocalDate TOMORROW = LocalDate.now().plusDays(1);
+	private static final LocalDate YESTERDAY = LocalDate.now().minusDays(1);
+	private static final LocalTime NINE = LocalTime.of(9, 0);
+	private static final LocalTime TWELVE = LocalTime.of(12, 0);
+	private static final LocalTime ONE = LocalTime.of(13, 0);
+	private static final LocalTime FOUR = LocalTime.of(16, 0);
+	private static final Recurrence NO_RECURRENCE = new Recurrence(false, RecurrencePattern.NONE);
+	private static final Recurrence DAILY_RECURRENCE = new Recurrence(true, RecurrencePattern.DAILY);
+	private static final Recurrence WEEKLY_RECURRENCE = new Recurrence(true, RecurrencePattern.WEEKLY);
+	private static final Recurrence BIWEEKLY_RECURRENCE = new Recurrence(true, RecurrencePattern.BIWEEKLY);
+	private static final Recurrence MONTHLY_RECURRENCE = new Recurrence(true, RecurrencePattern.MONTHLY);
 
 	@Test
 	@DisplayName("an exception should be thrown if the start time is after the end time.")
 	void checkReservationDateTimeShouldThrowIfStartTimeIsAfterBeginTime() {
-		assertThrows(InvalidStartAndEndTimeException.class, () ->
-				DateTimeUtils.checkReservationDateTime(this.today, this.twelve, this.nine));
+		assertThrows(InvalidStartAndEndTimeException.class,
+				() -> checkReservationDateTime(TODAY, TWELVE, NINE));
 	}
 
 	@Test
 	@DisplayName("an exception should be thrown if the date is before today")
 	void checkReservationDateTimeShouldThrowIfDateIsBeforeToday() {
-		assertThrows(InvalidDayException.class, () ->
-				DateTimeUtils.checkReservationDateTime(this.yesterday, this.nine, this.twelve));
+		assertThrows(InvalidDayException.class,
+				() -> checkReservationDateTime(YESTERDAY, NINE, TWELVE));
 	}
 
 	@Test
 	@DisplayName("no exception should be thrown if the dates and times are okay")
 	void checkReservationDateTimeShouldNotThrowIfDateAndTimeAreOkay() {
-		assertDoesNotThrow(() ->
-				DateTimeUtils.checkReservationDateTime(this.today, this.nine, this.twelve));
+		assertDoesNotThrow(() -> checkReservationDateTime(TODAY, NINE, TWELVE));
 	}
 
 	@Test
 	@DisplayName("when there are different input days, the function should return false")
 	void timeslotsDontOverlapIfTheyAreOnDifferentDays() {
-		assertFalse(DateTimeUtils.timeslotsOverlap(this.today, this.nine,
-				this.twelve, this.noRecurrence, this.tomorrow, this.nine, this.twelve));
+		assertFalse(timeslotsOverlap(TODAY, NINE, TWELVE, NO_RECURRENCE, TOMORROW, NINE, TWELVE));
 	}
 
+	//TODO: split up into multiple tests
 	@Test
 	@DisplayName("when there is recurrence, but the check is for a different time on that day, " +
 			"the function should return false")
 	void timeslotsDontOverlapIfTheyRecurOnDifferentTimes() {
-		assertFalse(DateTimeUtils.timeslotsOverlap(this.today, this.nine,
-				this.twelve, this.dailyRecurrence, this.tomorrow, this.one, this.four));
+		assertFalse(timeslotsOverlap(TODAY, NINE, TWELVE, DAILY_RECURRENCE, TOMORROW, ONE, FOUR));
 
-		assertFalse(DateTimeUtils.timeslotsOverlap(this.today, this.nine,
-				this.twelve, this.dailyRecurrence, this.tomorrow, this.one, this.four));
+		assertFalse(timeslotsOverlap(TODAY, NINE, TWELVE, DAILY_RECURRENCE, TOMORROW, ONE, FOUR));
 
 		LocalDate nextWeek = LocalDate.now().plusWeeks(1);
-		assertFalse(DateTimeUtils.timeslotsOverlap(this.today, this.nine,
-				this.twelve, this.weeklyRecurrence, nextWeek, this.one, this.four));
+		assertFalse(timeslotsOverlap(TODAY, NINE, TWELVE, WEEKLY_RECURRENCE, nextWeek, ONE, FOUR));
 
 		LocalDate twoWeeks = LocalDate.now().plusWeeks(2);
-		assertFalse(DateTimeUtils.timeslotsOverlap(this.today, this.nine,
-				this.twelve, this.biweeklyRecurrence, twoWeeks, this.one, this.four));
+		assertFalse(timeslotsOverlap(TODAY, NINE, TWELVE, BIWEEKLY_RECURRENCE, twoWeeks, ONE, FOUR));
 
 		twoWeeks = LocalDate.now().plusWeeks(2).plusDays(1);
-		assertFalse(DateTimeUtils.timeslotsOverlap(this.today, this.nine,
-				this.twelve, this.biweeklyRecurrence, twoWeeks, this.one, this.four));
+		assertFalse(timeslotsOverlap(TODAY, NINE, TWELVE, BIWEEKLY_RECURRENCE, twoWeeks, ONE, FOUR));
 
 		LocalDate month = LocalDate.now().plusMonths(1);
-		assertFalse(DateTimeUtils.timeslotsOverlap(this.today, this.nine,
-				this.twelve, this.monthlyRecurrence, month, this.one, this.four));
+		assertFalse(timeslotsOverlap(TODAY, NINE, TWELVE, MONTHLY_RECURRENCE, month, ONE, FOUR));
 
-		assertTrue(DateTimeUtils.timeslotsOverlap(LocalDate.now().plusDays(1),
+		assertTrue(timeslotsOverlap(LocalDate.now().plusDays(1),
 				LocalTime.of(11, 11), LocalTime.of(12, 12),
 				new Recurrence(true, RecurrencePattern.DAILY),
 				LocalDate.now().plusDays(0), LocalTime.of(11, 11), LocalTime.of(12, 12)));
