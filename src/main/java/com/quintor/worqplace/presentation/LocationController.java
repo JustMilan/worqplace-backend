047d2f5f6635c6e478508a3d251a29ff1df03daa
JustMilan <milandol321@gmail.com>
2021-12-24 18:56:39 +0100

    Remove unused DTO and mapper

--- src/main/java/com/quintor/worqplace/presentation/LocationController.java
+++ src/main/java/com/quintor/worqplace/presentation/LocationController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Controller for {@link com.quintor.worqplace.domain.Location locations}, contains logic
